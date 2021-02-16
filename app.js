0b58d0ec2f2f3e2270029177882c7f0f328b3d4c
karthick <karthylock@gmail.com>
2021-02-17 02:40:27 +0530

    Updates

--- app.js
+++ app.js
@@ -1,24 +1,78 @@
+/* eslint-disable no-useless-escape */
+/* eslint-disable no-undef */
 /******/ (() => { // webpackBootstrap
-/*!**********************!*\
-  !*** ./src/index.js ***!
-  \**********************/
-const loc = document.getElementById('location');
-const tempIcon = document.getElementById('temp-icon');
-const tempValue = document.getElementById('temp-value');
-const climate = document.getElementById('climate');
-const searchInput = document.getElementById('search-input');
-const searchButton = document.getElementById('search-button');
+/******/ 	"use strict";
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/global */
+/******/ 	(() => {
+/******/ 		__webpack_require__.g = (function() {
+/******/ 			if (typeof globalThis === 'object') return globalThis;
+/******/ 			try {
+/******/ 				return this || new Function('return this')();
+/******/ 			} catch (e) {
+/******/ 				if (typeof window === 'object') return window;
+/******/ 			}
+/******/ 		})();
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		var scriptUrl;
+/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
+/******/ 		var document = __webpack_require__.g.document;
+/******/ 		if (!scriptUrl && document) {
+/******/ 			if (document.currentScript)
+/******/ 				scriptUrl = document.currentScript.src
+/******/ 			if (!scriptUrl) {
+/******/ 				var scripts = document.getElementsByTagName("script");
+/******/ 				if(scripts.length) scriptUrl = scripts[scripts.length - 1].src
+/******/ 			}
+/******/ 		}
+/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
+/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
+/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
+/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
+/******/ 		__webpack_require__.p = scriptUrl;
+/******/ 	})();
+/******/ 	
+/************************************************************************/
 
-searchButton.addEventListener('click', (e) => {
+// CONCATENATED MODULE: ./src/images/storm.svg
+/* harmony default export */ const storm = (__webpack_require__.p + "images/storm.svg");
+// CONCATENATED MODULE: ./src/images/clouds.svg
+/* harmony default export */ const clouds = (__webpack_require__.p + "images/clouds.svg");
+// CONCATENATED MODULE: ./src/images/rain.svg
+/* harmony default export */ const rain = (__webpack_require__.p + "images/rain.svg");
+// CONCATENATED MODULE: ./src/images/snowflake.svg
+/* harmony default export */ const snowflake = (__webpack_require__.p + "images/snowflake.svg");
+// CONCATENATED MODULE: ./src/index.js
+
+
+
+
+
+
+
+const loc = document.getElementById("location");
+const tempIcon = document.getElementById("temp-icon");
+const tempValue = document.getElementById("temp-value");
+const climate = document.getElementById("climate");
+const searchInput = document.getElementById("search-input");
+const searchButton = document.getElementById("search-button");
+
+searchButton.addEventListener("click", (e) => {
   e.preventDefault();
   getWeather(searchInput.value);
-  searchInput.Value = '';
+  searchInput.Value = "";
 });
 
 const getWeather = async (city) => {
   try {
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=90582edac32da819f08413ad8ebbcb45`,
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=90582edac32da819f08413ad8ebbcb45`
     );
 
     const weatherData = await response.json();
@@ -33,24 +87,24 @@ const getWeather = async (city) => {
     tempValue.textContent = Math.round(feels_like - 273);
 
     if (id < 300 && id < 200) {
-      tempIcon.src = './images/storm.svg';
+      tempIcon.src = storm;
     } else if (id < 400 && id < 300) {
-      tempIcon.src = './images/clouds.svg';
+      tempIcon.src = clouds;
     } else if (id < 600 && id < 500) {
-      tempIcon.src = './images/rain.svg';
+      tempIcon.src = rain;
     } else if (id < 700 && id < 600) {
-      tempIcon.src = './images/snowflake.svg';
+      tempIcon.src = snowflake;
     } else if (id < 800 && id < 700) {
-      tempIcon.src = './images/clouds.svg';
+      tempIcon.src = clouds;
     } else if (id == 800) {
-      tempIcon.src = './images/clouds.svg';
+      tempIcon.src = clouds;
     }
   } catch (error) {
-     alert("City Not Found");
+    alert("City Not Found");
   }
 };
 
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
   let long;
   let lat;
 
@@ -72,19 +126,18 @@ window.addEventListener('load', () => {
           tempValue.textContent = Math.round(feels_like - 273);
 
           if (id < 300 && id < 200) {
-            tempIcon.src = './images/storm.svg';
+            tempIcon.src = storm;
           } else if (id < 400 && id < 300) {
-            tempIcon.src = './images/clouds.svg';
+            tempIcon.src = clouds;
           } else if (id < 600 && id < 500) {
-            tempIcon.src = './images/rain.svg';
+            tempIcon.src = rain;
           } else if (id < 700 && id < 600) {
-            tempIcon.src = './images/snowflake.svg';
+            tempIcon.src = snowflake;
           } else if (id < 800 && id < 700) {
-            tempIcon.src = './images/clouds.svg';
+            tempIcon.src = clouds;
           } else if (id == 800) {
-            tempIcon.src = './images/clouds.svg';
+            tempIcon.src = clouds;
           }
-         
         });
     });
   }
