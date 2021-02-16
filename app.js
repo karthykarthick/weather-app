c6549c050f09cfdf00a30561cf101a313c54e982
karthick <karthylock@gmail.com>
2021-02-17 04:32:10 +0530

    Updates

--- app.js
+++ app.js
@@ -1,55 +1,90 @@
 /* eslint-disable no-useless-escape */
 /* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 /******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
-/******/ 	// The require scope
-/******/ 	var __webpack_require__ = {};
-/******/ 	
-/************************************************************************/
-/******/ 	/* webpack/runtime/global */
-/******/ 	(() => {
-/******/ 		__webpack_require__.g = (function() {
-/******/ 			if (typeof globalThis === 'object') return globalThis;
-/******/ 			try {
-/******/ 				return this || new Function('return this')();
-/******/ 			} catch (e) {
-/******/ 				if (typeof window === 'object') return window;
-/******/ 			}
-/******/ 		})();
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/publicPath */
-/******/ 	(() => {
-/******/ 		var scriptUrl;
-/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
-/******/ 		var document = __webpack_require__.g.document;
-/******/ 		if (!scriptUrl && document) {
-/******/ 			if (document.currentScript)
-/******/ 				scriptUrl = document.currentScript.src
-/******/ 			if (!scriptUrl) {
-/******/ 				var scripts = document.getElementsByTagName("script");
-/******/ 				if(scripts.length) scriptUrl = scripts[scripts.length - 1].src
-/******/ 			}
-/******/ 		}
-/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
-/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
-/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
-/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
-/******/ 		__webpack_require__.p = scriptUrl;
-/******/ 	})();
-/******/ 	
-/************************************************************************/
+/******/ 	var __webpack_modules__ = ({
 
-// CONCATENATED MODULE: ./src/images/storm.svg
-/* harmony default export */ const storm = (__webpack_require__.p + "images/storm.svg");
-// CONCATENATED MODULE: ./src/images/clouds.svg
-/* harmony default export */ const clouds = (__webpack_require__.p + "images/clouds.svg");
-// CONCATENATED MODULE: ./src/images/rain.svg
-/* harmony default export */ const rain = (__webpack_require__.p + "images/rain.svg");
-// CONCATENATED MODULE: ./src/images/snowflake.svg
-/* harmony default export */ const snowflake = (__webpack_require__.p + "images/snowflake.svg");
-// CONCATENATED MODULE: ./src/index.js
+/***/ "./src/images/clouds.svg":
+/*!*******************************!*\
+  !*** ./src/images/clouds.svg ***!
+  \*******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + "images/clouds.svg");
+
+/***/ }),
+
+/***/ "./src/images/rain.svg":
+/*!*****************************!*\
+  !*** ./src/images/rain.svg ***!
+  \*****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + "images/rain.svg");
+
+/***/ }),
+
+/***/ "./src/images/snowflake.svg":
+/*!**********************************!*\
+  !*** ./src/images/snowflake.svg ***!
+  \**********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + "images/snowflake.svg");
+
+/***/ }),
+
+/***/ "./src/images/storm.svg":
+/*!******************************!*\
+  !*** ./src/images/storm.svg ***!
+  \******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + "images/storm.svg");
+
+/***/ }),
+
+/***/ "./src/scss/styles.scss":
+/*!******************************!*\
+  !*** ./src/scss/styles.scss ***!
+  \******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _scss_styles_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./scss/styles.scss */ "./src/scss/styles.scss");
+/* harmony import */ var _src_images_storm_svg__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../src/images/storm.svg */ "./src/images/storm.svg");
+/* harmony import */ var _src_images_clouds_svg__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../src/images/clouds.svg */ "./src/images/clouds.svg");
+/* harmony import */ var _src_images_rain_svg__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../src/images/rain.svg */ "./src/images/rain.svg");
+/* harmony import */ var _src_images_snowflake_svg__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../src/images/snowflake.svg */ "./src/images/snowflake.svg");
 
 
 
@@ -62,6 +97,31 @@ const tempValue = document.getElementById("temp-value");
 const climate = document.getElementById("climate");
 const searchInput = document.getElementById("search-input");
 const searchButton = document.getElementById("search-button");
+const temp = document.querySelector("#selectt");
+console.log(temp);
+
+function weatherConverter(feels_like) {
+  console.log(feels_like);
+  temp.addEventListener("click", (e) => {
+    let tempval = e.target.value;
+    console.log(tempval);
+    if (tempval == 1) {
+      var celcius = Math.round(feels_like - 273.15);
+      console.log(celcius);
+      tempValue.textContent = celcius + "°C";
+      console.log(tempval);
+      return celcius;
+    }else{
+      var faren = feels_like;
+      faren = (feels_like - 273.15) * 9/5 + 32;
+      faren = Math.round(faren);
+      console.log(faren);
+      tempValue.textContent = faren + "°F";
+      // console.log(round);
+      return faren;
+    }
+  });
+}
 
 searchButton.addEventListener("click", (e) => {
   e.preventDefault();
@@ -76,28 +136,28 @@ const getWeather = async (city) => {
     );
 
     const weatherData = await response.json();
-    console.log(weatherData);
 
     const { name } = weatherData;
     const { feels_like } = weatherData.main;
     const { id, main } = weatherData.weather[0];
-
     loc.textContent = name;
     climate.textContent = main;
-    tempValue.textContent = Math.round(feels_like - 273);
+    weatherConverter(feels_like);
+   
+    // tempValue.textContent = Math.round(feels_like - 273);
 
     if (id < 300 && id < 200) {
-      tempIcon.src = storm;
+      tempIcon.src = _src_images_storm_svg__WEBPACK_IMPORTED_MODULE_1__.default;
     } else if (id < 400 && id < 300) {
-      tempIcon.src = clouds;
+      tempIcon.src = _src_images_clouds_svg__WEBPACK_IMPORTED_MODULE_2__.default;
     } else if (id < 600 && id < 500) {
-      tempIcon.src = rain;
+      tempIcon.src = _src_images_rain_svg__WEBPACK_IMPORTED_MODULE_3__.default;
     } else if (id < 700 && id < 600) {
-      tempIcon.src = snowflake;
+      tempIcon.src = _src_images_snowflake_svg__WEBPACK_IMPORTED_MODULE_4__.default;
     } else if (id < 800 && id < 700) {
-      tempIcon.src = clouds;
+      tempIcon.src = _src_images_clouds_svg__WEBPACK_IMPORTED_MODULE_2__.default;
     } else if (id == 800) {
-      tempIcon.src = clouds;
+      tempIcon.src = _src_images_clouds_svg__WEBPACK_IMPORTED_MODULE_2__.default;
     }
   } catch (error) {
     alert("City Not Found");
@@ -123,26 +183,120 @@ window.addEventListener("load", () => {
 
           loc.textContent = name;
           climate.textContent = main;
-          tempValue.textContent = Math.round(feels_like - 273);
+          tempValue.textContent = Math.round(feels_like - 273) + "°C";
 
           if (id < 300 && id < 200) {
-            tempIcon.src = storm;
+            tempIcon.src = _src_images_storm_svg__WEBPACK_IMPORTED_MODULE_1__.default;
           } else if (id < 400 && id < 300) {
-            tempIcon.src = clouds;
+            tempIcon.src = _src_images_clouds_svg__WEBPACK_IMPORTED_MODULE_2__.default;
           } else if (id < 600 && id < 500) {
-            tempIcon.src = rain;
+            tempIcon.src = _src_images_rain_svg__WEBPACK_IMPORTED_MODULE_3__.default;
           } else if (id < 700 && id < 600) {
-            tempIcon.src = snowflake;
+            tempIcon.src = _src_images_snowflake_svg__WEBPACK_IMPORTED_MODULE_4__.default;
           } else if (id < 800 && id < 700) {
-            tempIcon.src = clouds;
+            tempIcon.src = _src_images_clouds_svg__WEBPACK_IMPORTED_MODULE_2__.default;
           } else if (id == 800) {
-            tempIcon.src = clouds;
+            tempIcon.src = _src_images_clouds_svg__WEBPACK_IMPORTED_MODULE_2__.default;
           }
         });
     });
   }
 });
 
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		if(__webpack_module_cache__[moduleId]) {
+/******/ 			return __webpack_module_cache__[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/global */
+/******/ 	(() => {
+/******/ 		__webpack_require__.g = (function() {
+/******/ 			if (typeof globalThis === 'object') return globalThis;
+/******/ 			try {
+/******/ 				return this || new Function('return this')();
+/******/ 			} catch (e) {
+/******/ 				if (typeof window === 'object') return window;
+/******/ 			}
+/******/ 		})();
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		var scriptUrl;
+/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
+/******/ 		var document = __webpack_require__.g.document;
+/******/ 		if (!scriptUrl && document) {
+/******/ 			if (document.currentScript)
+/******/ 				scriptUrl = document.currentScript.src
+/******/ 			if (!scriptUrl) {
+/******/ 				var scripts = document.getElementsByTagName("script");
+/******/ 				if(scripts.length) scriptUrl = scripts[scripts.length - 1].src
+/******/ 			}
+/******/ 		}
+/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
+/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
+/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
+/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
+/******/ 		__webpack_require__.p = scriptUrl;
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	// startup
+/******/ 	// Load entry module
+/******/ 	__webpack_require__("./src/index.js");
+/******/ 	// This entry module used 'exports' so it can't be inlined
 /******/ })()
 ;
 //# sourceMappingURL=app.js.map
