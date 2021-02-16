f66bbed3270d0de2b03d902cba1a6f16e63fcf96
karthick <karthylock@gmail.com>
2021-02-17 01:01:07 +0530

    added functionality

--- dist/app.js
+++ dist/app.js
@@ -12,6 +12,51 @@ let iconfile;
 const searchInput = document.getElementById("search-input");
 const searchButton = document.getElementById("search-button");
 
+
+searchButton.addEventListener("click", (e)=>{
+    e.preventDefault();
+    getWeather(searchInput.Value);
+    searchInput.Value='';
+});
+
+const getWeather = async (city) => {
+    try{
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=983e961e0a60ab799a3923d5c955726c`,
+        {mode: 'cors'}      
+        );
+
+        const weatherData = await response.json();
+        console.log(weatherData);
+
+        const { name } = data;
+        const { feels_like } = data.main;
+        const { id, main } = data.weather[0];
+
+        loc.textContent = name;
+        climate.textContent = main;
+        tempValue.textContent = Math.round(feels_like - 273);
+
+
+        if (id < 300 && id < 200) {
+            tempIcon.src = "./images/storm.svg";
+          } else if (id < 400 && id < 300) {
+            tempIcon.src = "./images/clouds.svg";
+          } else if (id < 600 && id < 500) {
+            tempIcon.src = "./images/rain.svg";
+          } else if (id < 700 && id < 600) {
+            tempIcon.src = "./images/snowflake.svg";
+          } else if (id < 800 && id < 700) {
+            tempIcon.src = "./images/clouds.svg";
+          } else if (id == 800) {
+            tempIcon.src = "./images/clouds.svg";
+          }
+    }
+    catch(error){
+        // alert("City Not Found");
+    }
+}
+
+
 window.addEventListener("load", () => {
   let long;
   let lat;
@@ -20,23 +65,38 @@ window.addEventListener("load", () => {
     navigator.geolocation.getCurrentPosition((position) => {
       long = position.coords.longitude;
       lat = position.coords.latitude;
-      const proxt = "https://cors-anywhere.herokuapp.com/";
+    //   const proxy = "https://cors-anywhere.herokuapp.com/";
       // const api = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=4b0acdbf1b451ce9f57778d2b016642b`;
-      const api = `${proxt}api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=4b0acdbf1b451ce9f57778d2b016642b`;
-  
-      fetch(api).then((response) => {
+      const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=4b0acdbf1b451ce9f57778d2b016642b`;
+
+      fetch(api)
+        .then((response) => {
           return response.json();
-        }).then((data) => {
+        })
+        .then((data) => {
           const { name } = data;
           const { feels_like } = data.main;
           const { id, main } = data.weather[0];
-  
+
           loc.textContent = name;
           climate.textContent = main;
           tempValue.textContent = Math.round(feels_like - 273);
-        });
 
-      
+          if (id < 300 && id < 200) {
+            tempIcon.src = "./images/storm.svg";
+          } else if (id < 400 && id < 300) {
+            tempIcon.src = "./images/clouds.svg";
+          } else if (id < 600 && id < 500) {
+            tempIcon.src = "./images/rain.svg";
+          } else if (id < 700 && id < 600) {
+            tempIcon.src = "./images/snowflake.svg";
+          } else if (id < 800 && id < 700) {
+            tempIcon.src = "./images/clouds.svg";
+          } else if (id == 800) {
+            tempIcon.src = "./images/clouds.svg";
+          }
+          console.log(data);
+        });
     });
   }
 });
